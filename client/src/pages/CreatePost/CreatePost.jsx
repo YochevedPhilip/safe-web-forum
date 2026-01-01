6b9a377cbad8a2382ab30112ba5a78a78a3f8b09
tifaret <tpnwnw@gmail.com>
2026-01-01 17:34:54 +0200

    Add Create Post page and button in HomePage

---
+++ client/src/pages/CreatePost/CreatePost.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CreatePost = () => {
+  return (
+    <div>
+      <h1>Create a New Post</h1>
+      <p>Welcome! Here you can start creating your new post.</p>
+    </div>
+  );
+};
+
+export default CreatePost;
